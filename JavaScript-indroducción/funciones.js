a3529cd8eb6f8841e8fb24d08a29454dbfbfceea
MiguelMurrugarraTorres <angel.16.92@hotmail.com>
2023-03-02 11:42:02 -0500

    avanzado el curso de Javascrip basico

---
+++ JavaScript-indroducción/funciones.js
@@ -0,0 +1,21 @@
+//Declarativa
+
+function miFuncion() {
+    return 3;
+}
+
+miFuncion();
+
+//Expresión
+
+var miFuncion2 = function(a,b){
+    return a + b;
+}
+
+miFuncion2();
+
+
+
+
+
+
---
+++ JavaScript-indroducción/variables.js
@@ -0,0 +1,21 @@
+var nombre = "oscar";
+
+//declarar
+
+var edad;
+
+//inicializar
+
+edad = 30 ;
+
+edad
+
+var elementos = [ "Computadora", "Celular" ];
+
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
+
+persona
+
