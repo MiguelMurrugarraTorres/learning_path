6b699ffa9cd273ad73f0cbdd2b05fa1e40b10486
MiguelMurrugarraTorres <Angel.16.92@hotmail.com>
2023-03-21 14:03:34 -0500

    condicionales

---
+++ JavaScript-indroducción/if.js
@@ -0,0 +1,10 @@
+
+var edad = 18;
+
+if(edad === 18){
+    console.log("hey sera tu primera vez");
+} else if(edad > 18){
+   console.log("puedes votar de nuevo");
+}else{
+    console.log("aun no puedes votar");
+}
