ffcce6c694c97a884a53606b9f01bbef272b2c0f
MiguelMurrugarraTorres <Angel.16.92@hotmail.com>
2023-03-21 12:42:32 -0500

    clase scope local y global

---
+++ JavaScript-indroducción/scope.js
@@ -0,0 +1,12 @@
+
+//scope global
+var nombre = "Miguel"
+
+
+//scope local
+function nombres(){
+    var apellido = "Murrugarra";
+    console.log(nombre + " " + apellido);   
+                      }
+
+nombres();
