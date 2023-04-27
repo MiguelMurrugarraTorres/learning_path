2035e1972d0418cb8654b3aac8ebb3b75408c274
MiguelMurrugarraTorres <Angel.16.92@hotmail.com>
2023-04-26 21:36:49 -0500

    operadores ejemplos

---
+++ JavaScript-indroducción/operadores aritmeticos
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 2;
+
+console.log(a + b);   // muestra 7 en la consola
+console.log(a - b);   // muestra 3 en la consola
+console.log(a * b);   // muestra 10 en la consola
+console.log(a / b);   // muestra 2.5 en la consola
+console.log(a % b);   // muestra 1 en la consola
---
+++ JavaScript-indroducción/operadoresComparación.js
@@ -0,0 +1,9 @@
+let x = 5;
+let y = "5";
+
+console.log(x == y);    // muestra true en la consola
+console.log(x === y);   // muestra false en la consola
+console.log(x != y);    // muestra false en la consola
+console.log(x !== y);   // muestra true en la consola
+console.log(x > 3);     // muestra true en la consola
+console.log(x <= 4);    // muestra false en la consola
