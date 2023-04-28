1dd6d86db644e494c735bb24cdd4198847815f04
MiguelMurrugarraTorres <Angel.16.92@hotmail.com>
2023-04-28 13:32:03 -0500

    Los loops o bucles son estructuras de control de flujo que permiten repetir un bloque de código varias veces, hasta que se cumpla una determinada condición

---
+++ JavaScript-indroducción/loop_while.js
@@ -0,0 +1,11 @@
+let estudiantes = ["JUAN", "MIGUEL", "JOSE", "HOBERTT"];
+
+while (estudiantes.length > 0) {
+  console.log(estudiantes.shift());
+}
+
+//Resultado:
+// JUAN
+// MIGUEL
+// JOSE
+// HOBERTT
