936e07a2c1b8945ae8eadc02916df7df617cdabe
MiguelMurrugarraTorres <Angel.16.92@hotmail.com>
2023-06-15 19:30:30 -0500

    scaffold es un widget predefinido que proporciona una estructura básica para construir una interfaz de usuario

--- FLUTTER/counter_app/lib/screens/home_screen.dart
+++ FLUTTER/counter_app/lib/screens/home_screen.dart
@@ -6,7 +6,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-        backgroundColor: Colors.indigo,
+        backgroundColor: Colors.pink,
         body: Center(
           child: Text('HomeScreen'),
         ));
