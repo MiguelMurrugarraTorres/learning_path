b4d9857e3f8ecb0fe40ac6de24f02b7cf6ba6712
MiguelMurrugarraTorres <Angel.16.92@hotmail.com>
2023-06-15 20:30:30 -0500

    BuildContext es un objeto que proporciona información sobre la ubicación actual dentro del árbol de widgets de Flutter.

--- FLUTTER/counter_app/lib/screens/home_screen.dart
+++ FLUTTER/counter_app/lib/screens/home_screen.dart
@@ -6,7 +6,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-        backgroundColor: Colors.pink,
+        backgroundColor: Colors.indigo,
         body: Center(
           child: Text('HomeScreen'),
         ));
