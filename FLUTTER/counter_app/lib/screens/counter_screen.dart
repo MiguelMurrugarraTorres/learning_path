f96e126678649a1e6df860a7f0b59788ce54234f
MiguelMurrugarraTorres <Angel.16.92@hotmail.com>
2023-06-16 22:55:30 -0500

    creación de otro archivo counter_screen donde vamos utilizar StatefulWidget para cambio de estado en el counter incrementado cada vez que damos tap al boton flotante

---
+++ FLUTTER/counter_app/lib/screens/counter_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  //variable, propiedad
+  int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    const fontSize30 = TextStyle(fontSize: 30);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CounterScreem'),
+        elevation: 0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Number de clicks', style: fontSize30),
+            Text(
+              '$counter',
+              style: fontSize30,
+            )
+          ],
+        ),
+      ),
+      //floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              counter++;
+            });
+          }),
+    );
+  }
+}
