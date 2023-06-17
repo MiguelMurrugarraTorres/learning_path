0bc05eeed5a8d2002fb7f2119bf863f6d082675f
MiguelMurrugarraTorres <Angel.16.92@hotmail.com>
2023-06-16 22:50:30 -0500

    se cambio la ubicación de boton flotante

--- FLUTTER/counter_app/lib/screens/home_screen.dart
+++ FLUTTER/counter_app/lib/screens/home_screen.dart
@@ -23,6 +23,7 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton:
           FloatingActionButton(child: const Icon(Icons.add), onPressed: () {}),
     );
