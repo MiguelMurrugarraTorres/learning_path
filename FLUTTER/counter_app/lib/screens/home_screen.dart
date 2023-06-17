3597fab67b44fccfaf6f7e3cb7e4124733cefdff
MiguelMurrugarraTorres <Angel.16.92@hotmail.com>
2023-06-16 22:55:30 -0500

    creación de otro archivo counter_screen donde vamos utilizar StatefulWidget para cambio de estado en el counter incrementado cada vez que damos tap al boton flotante

--- FLUTTER/counter_app/lib/screens/home_screen.dart
+++ FLUTTER/counter_app/lib/screens/home_screen.dart
@@ -3,9 +3,12 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  //variable, propiedad
+
   @override
   Widget build(BuildContext context) {
     const fontSize30 = TextStyle(fontSize: 30);
+    int counter = 0;
     return Scaffold(
       appBar: AppBar(
         title: const Text('HomeScreen'),
@@ -14,18 +17,22 @@ class HomeScreen extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text('Number de clicks', style: fontSize30),
+          children: [
+            const Text('Number de clicks', style: fontSize30),
             Text(
-              '10',
+              '$counter',
               style: fontSize30,
             )
           ],
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton:
-          FloatingActionButton(child: const Icon(Icons.add), onPressed: () {}),
+      //floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            counter++;
+            print('Hola Mundo: $counter');
+          }),
     );
   }
 }
